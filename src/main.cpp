55dba514bbcb2c33ac566d6ed2b02fb5fa3f9d65
RandoChoi <136892056@qq.com>
2018-07-24 16:17:39 +0800

    merge algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@
 //  Created by Daxin Wang on 2018/7/15.
 //  Copyright © 2018 Daxin Wang. All rights reserved.
 //
+#include <iostream>
+#include <assert.h>
+
+//pcl
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 
 #include "../include/octomap_rrt.hpp"
 #include "../include/map.hpp"
@@ -15,10 +21,64 @@ using namespace std;
 using namespace octomap;
 
 int main(int argc, char** argv) {
-    Map* map = new Map("./hello_2.bt");
+
+    if (argc != 3)
+    {
+        cout<<"Usage: pcd2octomap <input_file> <output_file>"<<endl;
+        return -1;
+    }
+
+    string input_file = argv[1], output_file = argv[2];
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    pcl::io::loadPCDFile<pcl::PointXYZ> ( input_file, cloud );
+
+    cout<<"point cloud loaded, piont size = "<<cloud.points.size()<<endl;
+    
+    //声明octomap变量
+    cout<<"copy data into octomap..."<<endl;
+    
+    //define the bounding box
+    octomap::point3d max;
+    octomap::point3d min;
+    max.x()=3;
+    max.y()=3;
+    max.z()=3;
+    min.x()=-3;
+    min.y()=-3;
+    min.z()=-3;
+
+    double resolution=0.005;
+
+    // 创建八叉树对象，参数为分辨率，这里设成了0.05
+    octomap::OcTree tree(resolution);
+
+    tree.setBBXMax(max);
+    tree.setBBXMin(min);
+
+    for (auto p:cloud.points)
+    {
+        // 将点云里的点插入到octomap中
+        //if(p.x<max.x()&&p.y<max.y()&&p.z<max.z()){
+        octomap::point3d temp(p.x,p.y,p.z);
+        if(tree.inBBX(temp)){
+            tree.updateNode( octomap::point3d(p.x, p.y, p.z),true);
+            //cout<<"("<<p.x<<","<<p.y<<","<<p.z<<")"<<endl;
+        }
+
+        
+        //}
+    }
+  
+    // 更新octomap
+    tree.updateInnerOccupancy();
+    // 存储octomap
+    tree.writeBinary( output_file );
+    cout<<"done."<<endl;
+
+    Map* map = new Map("./pcd_output/guidance_0.bt");
     octomap::point3d start_position(0,0,0);
-    octomap::point3d end_position(39,20,20);
-    RRT3D rrt(start_position, end_position, map, 60000, 2);
+    octomap::point3d end_position(1,1,1);
+    RRT3D rrt(start_position, end_position, map, 10000, 2);
     rrt.run();
     rrt.writeMap();
     map->writeFile("./path_octotree.bt");
